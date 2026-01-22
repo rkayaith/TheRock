9ebb877f972e8fc204f252fa1e045c857a4b4ccf
Michael Obersteiner <Michael.Obersteiner@amd.com>
2026-01-22 08:21:17 +0000

    feat: run libhipcxx tests for windows

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -429,7 +429,7 @@ test_matrix = {
         "fetch_artifact_args": "--libhipcxx --tests",
         "timeout_minutes": 30,
         "test_script": f"python {_get_script_path('test_libhipcxx_hipcc.py')}",
-        "platform": ["linux"],
+        "platform": ["linux", "windows"],
         "total_shards_dict": {
             "linux": 1,
             "windows": 1,
@@ -441,7 +441,7 @@ test_matrix = {
         "fetch_artifact_args": "--libhipcxx --tests",
         "timeout_minutes": 20,
         "test_script": f"python {_get_script_path('test_libhipcxx_hiprtc.py')}",
-        "platform": ["linux"],
+        "platform": ["linux", "windows"],
         "total_shards_dict": {
             "linux": 1,
             "windows": 1,
